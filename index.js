3982e969bfc404452e2c1caa56124cb04d753f12
Edil Soto <edil.soto@hunter.do>
2022-11-09 09:55:27 -0400

    pre Webpack

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 const body = document.body;
 
-const grid = document.querySelector('#principal');
+const grid = document.querySelector('.grid-container');
 
 
 
 const crearTimer = (dias, horas, minutos, segundos) => {
 
-const html = `
-        <h1 class="timer">${dias}</h1> 
-        <h1 class="timer">${horas}</h1>
-        <h1 class="timer">${minutos}</h1>
-        <h1 class="timer">${segundos}</h1>
+        const html = `
+        <div class="timer">
+        <h1>${dias}</h1> 
+        </div>
+        <div class="timer">
+        <h1>${horas}</h1>
+        </div>
+        <div class="timer">
+        <h1>${minutos}</h1>
+        </div>
+        <div class="timer">
+        <h1>${segundos}</h1>
+        </div>
         <h1>Dias</h1>
         <h1>Horas</h1>
         <h1>Minutos</h1>
         <h1>Segundos</h1>
 `;
 
-grid.innerHTML = html;
+        grid.innerHTML = html;
 };
 
 
